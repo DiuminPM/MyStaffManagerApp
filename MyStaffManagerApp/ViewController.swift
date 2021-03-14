439e8a86f0234364896a8468f4443f9c16e335c9
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-03-14 21:27:03 +0300

    Initial Commit

---
+++ MyStaffManagerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStaffManagerApp
+//
+//  Created by DiuminPM on 14.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
